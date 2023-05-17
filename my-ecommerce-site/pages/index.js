e51131e57374838dca67cafb89eee56fde321133
ColinHammarberg <colin.hammarberg2@gmail.com>
2023-05-17 21:10:07 +0200

    saved

--- my-ecommerce-site/pages/index.js
+++ my-ecommerce-site/pages/index.js
@@ -1,15 +1,35 @@
-export default function Home() {
-    return (
-      <div>
-        <div>
-          <title></title>
-        </div>
-        <div>
-          <h1></h1>
-        </div>
-        <div>
-        </div>
-      </div>
-    );
-  }
+import { request } from "../lib/datocms";
+
+const HOMEPAGE_QUERY = `{
+    allBoutiques {
+      id
+      name
+      boutiqueDetail {
+        ...on HeaderIdRecord{
+          smallTitle,
+          bigTitle,
+          buttonText,
+          description
+        }
+      }
+    }
+  
+    _allBoutiquesMeta {
+      count
+    }
+  }`;
+
+export async function getStaticProps() {
+  const data = await request({
+    query: HOMEPAGE_QUERY,
+    variables: { limit: 10 }
+  });
+  return {
+    props: { data }
+  };
+}
+export default function Home({ data }) {
+  console.log('data', data);
+  return <div>{JSON.stringify(data, null, 2)}</div>;
+}
   
